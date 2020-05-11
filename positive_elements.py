21f5d979d54612046a0b4eb211ad457cbe2b30c0
noopur0210 <64369657+noopur0210@users.noreply.github.com>
2020-05-11 12:01:01 +0530

    Create positive_elements.py
    This program prints the positive elements in the two given lists - list1 and list2

---
+++ positive_elements.py
@@ -0,0 +1,15 @@
+list1 = [12, -7, 5, 64, -14]
+list2 = [12, 14, -95, 3]
+i = 0
+print("Positive characters in list 1:")
+while i < 5:
+    if list1[i] > 0:
+        print(list1[i])
+    i += 1
+
+j = 0
+print("Positive characters in list 2:")
+while j < 4:
+    if list2[j] > 0:
+        print(list2[j])
+    j += 1
