8b385aa42d8da3130f42ded69925bf1a62b73cc2
noopur0210 <64369657+noopur0210@users.noreply.github.com>
2020-05-11 11:53:54 +0530

    Create fibonnaci.py

---
+++ fibonnaci.py
@@ -0,0 +1,13 @@
+first = 0
+second = 1
+sum = 0
+i = 1
+n = int(input("Enter the number of terms in series"))
+print(first)
+print(second)
+while i < n-1:
+    sum = first + second
+    first = second
+    second = sum
+    print(sum)
+    i+=1
